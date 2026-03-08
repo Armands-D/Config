95a4f37f3818a1752eeff7ba56f28e73e575ffc8
Armands Dunskis <armands.dunskis@gmail.com>
2026-03-08 17:14:21 +0000

    changed telescope treesitter remap

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -5,7 +5,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fm', builtin.treesitter, { desc = 'Telescope treesitter functions' })
+vim.keymap.set('n', '<leader>fs', builtin.treesitter, { desc = 'Telescope treesitter functions' }) -- find symbol
 
 -- # Harpoon
 local harpoon_ui = require('harpoon.ui')
