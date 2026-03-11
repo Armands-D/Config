9c5c68adeda12b570f3b75f3afd9910efda76913
Armands Dunskis <armands.dunskis@gmail.com>
2026-03-11 21:59:48 +0000

    nvim: replace ranger with telescope file browser, added satus line

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -7,6 +7,9 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fs', builtin.treesitter, { desc = 'Telescope treesitter functions' }) -- find symbol
 
+-- nvim-telescope/telescope-file-browser.nvim
+vim.keymap.set("n", "<space>ef", ":Telescope file_browser<CR>")
+
 -- # Harpoon
 local harpoon_ui = require('harpoon.ui')
 local harpoon_mark = require('harpoon.mark')
@@ -17,12 +20,12 @@ vim.keymap.set('n', '<leader>M', harpoon_mark.rm_file)
 -- vim.keymap.set('n', 'k', harpoon_ui.nav_prev)
 
 -- # Ranger
-vim.api.nvim_set_keymap("n", "<leader>ef", "", {
-  noremap = true,
-  callback = function()
-    require("ranger-nvim").open(true)
-  end,
-})
+--vim.api.nvim_set_keymap("n", "<leader>ef", "", {
+--  noremap = true,
+--  callback = function()
+--    require("ranger-nvim").open(true)
+--  end,
+--})
 
 -- # TreeSitter
 vim.api.nvim_create_autocmd('FileType', {
--- nvim/.config/nvim/lua/plugins/ranger.lua
+++ nvim/.config/nvim/lua/plugins/ranger.lua
@@ -1,15 +1,55 @@
+--return {
+--  "kelly-lin/ranger.nvim",
+--  config = function()
+--    require("ranger-nvim").setup({
+--        replace_netrw = true,
+--        ui = {
+--          border = "none",
+--          height = 1,
+--          width = 1,
+--          x = 0.5,
+--          y = 0.5,
+--        }
+--    })
+--  end,
+--}
+
 return {
-  "kelly-lin/ranger.nvim",
-  config = function()
-    require("ranger-nvim").setup({
-        replace_netrw = true,
-        ui = {
-          border = "none",
-          height = 1,
-          width = 1,
-          x = 0.5,
-          y = 0.5,
-        }
-    })
-  end,
+  "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = {
+    {
+     'nvim-telescope/telescope.nvim', version = '*',
+      dependencies = {
+          'nvim-lua/plenary.nvim',
+      }
+    }
+  },
+  opts={},
+  config = function ()
+    require("telescope").setup {
+      extensions = {
+        file_browser = {
+          browse_files = require("telescope._extensions.file_browser.finders").browse_files,
+          browse_folders = require("telescope._extensions.file_browser.finders").browse_folders,
+          -- disables netrw and use telescope-file-browser in its place
+          hidden = { file_browser = true, folder_browser = true},
+          respect_gitignore = vim.fn.executable "fd" == 1,
+          files = true,
+          add_dirs = true,
+          depth = 2,
+          no_ignore = true,
+          use_fd = true,
+          git_status = true,
+          hijack_netrw = true,
+          mappings = {
+            ["i"] = {
+            },
+            ["n"] = {
+            },
+          },
+        },
+      },
+    }
+    require("telescope").load_extension "file_browser"
+  end
 }
