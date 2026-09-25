e1564db59626af0c12acd240717dedfc81e92467
Armands Dunskis <armands.dunskis@gmail.com>
2026-09-25 01:55:02 +0100

    nvim: replace centerpad with zenmode

--- nvim/.config/nvim/lua/plugins/centerpad.lua
+++ nvim/.config/nvim/lua/plugins/centerpad.lua
@@ -5,8 +5,8 @@ return {
   },
   config = function ()
     -- using the command
-    vim.api.nvim_set_keymap('n', '<leader>z', '<cmd>Centerpad<cr>', { silent = true, noremap = true })
+    -- vim.api.nvim_set_keymap('n', '<leader>z', '<cmd>Centerpad<cr>', { silent = true, noremap = true })
     -- or using the lua function
-    vim.api.nvim_set_keymap('n', '<leader>z', "<cmd>lua require'centerpad'.toggle{ leftpad = 50, rightpad = 50 }<cr>", { silent = true, noremap = true })
+    -- vim.api.nvim_set_keymap('n', '<leader>z', "<cmd>lua require'centerpad'.toggle{ leftpad = 50, rightpad = 50 }<cr>", { silent = true, noremap = true })
   end
 }
